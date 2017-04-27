3233f0bc1cb96920715414744dbe998d97655240
Salvador Martinez-Bartolome <Salvador Martinez-Bartolome>
2017-04-27 11:39:53 -0700

    New OmimException Used for when API key is not valid

---
+++ pint.annotations/src/main/java/edu/scripps/yates/annotations/omim/OmimException.java
@@ -0,0 +1,13 @@
+package edu.scripps.yates.annotations.omim;
+
+public class OmimException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7247139465647253649L;
+
+	public OmimException(String message) {
+		super(message);
+	}
+}
