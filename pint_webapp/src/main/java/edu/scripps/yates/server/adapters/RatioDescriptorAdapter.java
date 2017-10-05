1ddb662af52693029e64209f6c35e890793152e4
Salvador Martinez-Bartolome <salvador@scripps.edu>
2017-10-05 11:08:15 -0700

    get ratio score name in the ratioDescriptorBean

--- pint_webapp/src/main/java/edu/scripps/yates/server/adapters/RatioDescriptorAdapter.java
+++ pint_webapp/src/main/java/edu/scripps/yates/server/adapters/RatioDescriptorAdapter.java
@@ -1,5 +1,8 @@
 package edu.scripps.yates.server.adapters;
 
+import edu.scripps.yates.proteindb.persistence.mysql.PeptideRatioValue;
+import edu.scripps.yates.proteindb.persistence.mysql.ProteinRatioValue;
+import edu.scripps.yates.proteindb.persistence.mysql.PsmRatioValue;
 import edu.scripps.yates.proteindb.persistence.mysql.RatioDescriptor;
 import edu.scripps.yates.proteindb.persistence.mysql.adapter.Adapter;
 import edu.scripps.yates.shared.model.RatioDescriptorBean;
@@ -21,6 +24,27 @@ public class RatioDescriptorAdapter implements Adapter<RatioDescriptorBean> {
 		ret.setCondition2Name(descriptor.getConditionByExperimentalCondition2Id().getName());
 		ret.setProjectTag(descriptor.getConditionByExperimentalCondition1Id().getProject().getTag());
 		ret.setRatioName(descriptor.getDescription());
+
+		if (descriptor.getProteinRatioValues() != null && !descriptor.getProteinRatioValues().isEmpty()) {
+			ProteinRatioValue proteinRatioValue = (ProteinRatioValue) descriptor.getProteinRatioValues().iterator()
+					.next();
+			if (proteinRatioValue.getConfidenceScoreName() != null) {
+				ret.setProteinScoreName(proteinRatioValue.getConfidenceScoreName());
+			}
+		}
+		if (descriptor.getPeptideRatioValues() != null && !descriptor.getPeptideRatioValues().isEmpty()) {
+			PeptideRatioValue peptideRatioValue = (PeptideRatioValue) descriptor.getPeptideRatioValues().iterator()
+					.next();
+			if (peptideRatioValue.getConfidenceScoreName() != null) {
+				ret.setPeptideScoreName(peptideRatioValue.getConfidenceScoreName());
+			}
+		}
+		if (descriptor.getPsmRatioValues() != null && !descriptor.getPsmRatioValues().isEmpty()) {
+			PsmRatioValue psmRatioValue = (PsmRatioValue) descriptor.getPsmRatioValues().iterator().next();
+			if (psmRatioValue.getConfidenceScoreName() != null) {
+				ret.setPsmScoreName(psmRatioValue.getConfidenceScoreName());
+			}
+		}
 		ret.setAggregationLevel(aggregationLevel);
 		return ret;
 	}
