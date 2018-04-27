285e612f4232e295fa8b68f45acf2ef6c1abacb5
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2018-04-27 13:35:09 -0700

    toString code

--- pint.annotations/src/main/java/edu/scripps/yates/annotations/uniprot/UniprotCVTermCode.java
+++ pint.annotations/src/main/java/edu/scripps/yates/annotations/uniprot/UniprotCVTermCode.java
@@ -3,9 +3,10 @@ package edu.scripps.yates.annotations.uniprot;
 public enum UniprotCVTermCode {
 
 	ID("Identifier (FT description)"), AC("Accession (PTM-xxxx)"), FT("Feature key"), TG("Target"), PA(
-			"Position of the modification on the amino acid"), PP("Position of the modification in the polypeptide"), CF(
-					"Correction formula"), MM("Monoisotopic mass difference"), MA("Average mass difference"), LC(
-							"Cellular location"), TR("Taxonomic range"), KW("Keyword"), DR("Cross-reference to PTM databases");
+			"Position of the modification on the amino acid"), PP(
+					"Position of the modification in the polypeptide"), CF("Correction formula"), MM(
+							"Monoisotopic mass difference"), MA("Average mass difference"), LC("Cellular location"), TR(
+									"Taxonomic range"), KW("Keyword"), DR("Cross-reference to PTM databases");
 
 	private final String content;
 	public final static String ENTRY_END = "//";
@@ -19,6 +20,11 @@ public enum UniprotCVTermCode {
 	 * @return the content
 	 */
 	public String getContent() {
+		return content;
+	}
+
+	@Override
+	public String toString() {
 		return content;
 	}
 
