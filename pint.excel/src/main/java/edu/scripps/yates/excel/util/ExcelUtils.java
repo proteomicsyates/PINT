4f43ed5f9342029cb8564b5da391838e7e8678dd
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2018-07-30 15:00:54 -0700

    new util method to get a list of the lines of the excel file

--- pint.excel/src/main/java/edu/scripps/yates/excel/util/ExcelUtils.java
+++ pint.excel/src/main/java/edu/scripps/yates/excel/util/ExcelUtils.java
@@ -1,5 +1,7 @@
 package edu.scripps.yates.excel.util;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -8,6 +10,9 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 
+import edu.scripps.yates.excel.ExcelColumn;
+import edu.scripps.yates.excel.ExcelSheet;
+import edu.scripps.yates.excel.impl.ExcelFileImpl;
 import edu.scripps.yates.model.util.PTMAdapter;
 import edu.scripps.yates.model.util.PTMSiteAdapter;
 import edu.scripps.yates.utilities.fasta.FastaParser;
@@ -39,49 +44,49 @@ public class ExcelUtils {
 	// }
 
 	public static List<PTM> getPTMsFromRawSequence(String seq) {
-		TDoubleObjectHashMap<PTM> map = new TDoubleObjectHashMap<PTM>();
+		final TDoubleObjectHashMap<PTM> map = new TDoubleObjectHashMap<PTM>();
 		if (FastaParser.somethingExtrangeInSequence(seq)) {
-			List<StringPosition> massDiffsStrings = FastaParser.getInside(seq);
+			final List<StringPosition> massDiffsStrings = FastaParser.getInside(seq);
 			if (!massDiffsStrings.isEmpty()) {
-				for (StringPosition stringMassDiffNumber : massDiffsStrings) {
+				for (final StringPosition stringMassDiffNumber : massDiffsStrings) {
 					// get the PTM
 					String aa = "";
 					if (stringMassDiffNumber.position > 0)
 						aa = String.valueOf(
 								appendList(FastaParser.getOutside(seq)).charAt(stringMassDiffNumber.position - 1));
 					try {
-						double massDiff = Double.valueOf(stringMassDiffNumber.string);
+						final double massDiff = Double.valueOf(stringMassDiffNumber.string);
 						if (!map.containsKey(massDiff)) {
 
-							PTM ptm = new PTMAdapter(massDiff, aa, stringMassDiffNumber.position).adapt();
+							final PTM ptm = new PTMAdapter(massDiff, aa, stringMassDiffNumber.position).adapt();
 							map.put(massDiff, ptm);
 						} else {
 							// add a ptm site
 							final PTMEx ptm = (PTMEx) map.get(massDiff);
 							ptm.addPtmSite(new PTMSiteAdapter(aa, stringMassDiffNumber.position).adapt());
 						}
-					} catch (NumberFormatException e) {
+					} catch (final NumberFormatException e) {
 						log.info(stringMassDiffNumber + " cannot be parsed as a number. " + e.getMessage());
 					}
 				}
 			}
 		}
-		List<PTM> ret = new ArrayList<PTM>();
-		for (PTM ptm : map.valueCollection()) {
+		final List<PTM> ret = new ArrayList<PTM>();
+		for (final PTM ptm : map.valueCollection()) {
 			ret.add(ptm);
 		}
 		Collections.sort(ret, new Comparator<PTM>() {
 
 			@Override
 			public int compare(PTM ptm1, PTM ptm2) {
-				int pos1 = getMinPTMPosition(ptm1);
-				int pos2 = getMinPTMPosition(ptm2);
+				final int pos1 = getMinPTMPosition(ptm1);
+				final int pos2 = getMinPTMPosition(ptm2);
 				return Integer.compare(pos1, pos2);
 			}
 
 			private int getMinPTMPosition(PTM ptm1) {
 				int min = Integer.MAX_VALUE;
-				for (PTMSite site : ptm1.getPTMSites()) {
+				for (final PTMSite site : ptm1.getPTMSites()) {
 					if (min > site.getPosition())
 						min = site.getPosition();
 				}
@@ -92,8 +97,8 @@ public class ExcelUtils {
 	}
 
 	private static String appendList(List<String> list) {
-		StringBuffer sb = new StringBuffer();
-		for (String string : list) {
+		final StringBuffer sb = new StringBuffer();
+		for (final String string : list) {
 			sb.append(string);
 		}
 		return sb.toString();
@@ -137,4 +142,28 @@ public class ExcelUtils {
 	// }
 	//
 	// }
+	public static List<String> getLines(File excelFile, String separator) throws IOException {
+		final List<String> ret = new ArrayList<String>();
+		final ExcelFileImpl reader = new ExcelFileImpl(excelFile);
+		final ExcelSheet excelSheet = reader.getSheets().get(0);
+		final List<String> columnHeaders = excelSheet.getColumnHeaders();
+		StringBuilder sb = new StringBuilder();
+		for (final String header : columnHeaders) {
+			sb.append(header).append(separator);
+		}
+		ret.add(sb.toString());
+
+		final int maxRow = excelSheet.getColumn(columnHeaders.get(0)).getLastNonEmptyRow();
+		for (int row = 1; row <= maxRow; row++) {
+			sb = new StringBuilder();
+			for (final String header : columnHeaders) {
+				final ExcelColumn column = excelSheet.getColumn(header);
+				final List<Object> values = column.getValues();
+				final Object object = values.get(row);
+				sb.append(object.toString()).append(separator);
+			}
+			ret.add(sb.toString());
+		}
+		return ret;
+	}
 }
