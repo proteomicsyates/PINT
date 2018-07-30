52c0d20e0a9a00cddf86f75895a3161343f179fa
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2018-07-30 15:18:08 -0700

    new util methods to extract info from uniprot entry XML

--- pint.annotations/src/main/java/edu/scripps/yates/annotations/util/UniprotEntryUtil.java
+++ pint.annotations/src/main/java/edu/scripps/yates/annotations/util/UniprotEntryUtil.java
@@ -1,7 +1,9 @@
 package edu.scripps.yates.annotations.util;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 import edu.scripps.yates.annotations.uniprot.xml.CommentType;
 import edu.scripps.yates.annotations.uniprot.xml.DbReferenceType;
@@ -12,7 +14,14 @@ import edu.scripps.yates.annotations.uniprot.xml.GeneNameType;
 import edu.scripps.yates.annotations.uniprot.xml.GeneType;
 import edu.scripps.yates.annotations.uniprot.xml.PropertyType;
 import edu.scripps.yates.annotations.uniprot.xml.ProteinType.SubmittedName;
+import edu.scripps.yates.annotations.uniprot.xml.SubcellularLocationType;
 import edu.scripps.yates.annotations.uniprot.xml.Uniprot;
+import gnu.trove.set.hash.THashSet;
+import uk.ac.ebi.kraken.interfaces.go.GoCategory;
+import uk.ac.ebi.kraken.interfaces.go.GoTerm;
+import uk.ac.ebi.kraken.model.go.GoIdImpl;
+import uk.ac.ebi.kraken.model.go.GoTermImpl;
+import uk.ac.ebi.kraken.model.go.GoTermNameImpl;
 
 public class UniprotEntryUtil {
 	public static void removeNonUsedElements(Uniprot uniprot, boolean removeReferences, boolean removeDBReferences) {
@@ -228,5 +237,128 @@ public class UniprotEntryUtil {
 			}
 		}
 		return null;
+	}
+
+	public static List<String> getCellularLocations(Entry entry) {
+		final List<String> ret = new ArrayList<String>();
+		if (entry != null) {
+			if (entry.getComment() != null) {
+				for (final CommentType comment : entry.getComment()) {
+					if (comment.getType() != null && "subcellular location".equals(comment.getType())) {
+						final List<SubcellularLocationType> subcellularLocations = comment.getSubcellularLocation();
+						if (subcellularLocations != null) {
+							for (final SubcellularLocationType subcellularLocation : subcellularLocations) {
+								final List<EvidencedStringType> locations = subcellularLocation.getLocation();
+								if (locations != null) {
+									for (final EvidencedStringType evidencedStringType : locations) {
+										if (!ret.contains(evidencedStringType.getValue())) {
+											ret.add(evidencedStringType.getValue());
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return ret;
+	}
+
+	/**
+	 * Retrieves the GO ID terms of all the cellularLocations
+	 * 
+	 * @param entry
+	 * @return
+	 */
+	public static Set<GoTerm> getGOTermsForCellularLocations(Entry entry) {
+		return getGOTermsForCellularLocations(entry, null);
+	}
+
+	private static List<PropertyType> filterPropertiesByType(Collection<PropertyType> properties, String type) {
+		final List<PropertyType> ret = new ArrayList<PropertyType>();
+		for (final PropertyType propertyType : properties) {
+			if (propertyType.getType().equals(type)) {
+				ret.add(propertyType);
+			}
+		}
+		return ret;
+	}
+
+	/**
+	 * Retrieves the GO ID terms of all the cellularLocations that are from an
+	 * specific source.<br>
+	 * 
+	 * 
+	 * @param entry
+	 * @param sources
+	 *            any list of sources. Valid sources can be UniProtKB,
+	 *            UniProtKB-KW, Ensembl, Reactome, UniProtKB-SubCell, HPA,
+	 *            ProtInc, or NULL if you want any source to be considered.<br>
+	 *            According to Uniprot webpage:
+	 *            <ul>
+	 *            <li>"UniprotKB" source is inferred from direct assay,</li>
+	 *            <li>"UniprotKB-SubCell","UniprotKB-KW" and "Ensembl" are
+	 *            inferred from electronic annotation,</li>
+	 *            <li>"Reactome" is traceable from author statement.</li>
+	 *            </ul>
+	 * @return
+	 */
+	public static Set<GoTerm> getGOTermsForCellularLocations(Entry entry, String... sources) {
+		final Set<GoTerm> ret = new THashSet<GoTerm>();
+		if (entry != null) {
+			if (entry.getDbReference() != null) {
+				for (final DbReferenceType dbReference : entry.getDbReference()) {
+					if (dbReference.getType() != null && "GO".equals(dbReference.getType())) {
+						final List<PropertyType> properties = dbReference.getProperty();
+						if (properties != null) {
+							final List<PropertyType> propertiesTerm = filterPropertiesByType(properties, "term");
+							final List<PropertyType> propertiesProject = filterPropertiesByType(properties, "project");
+							for (final PropertyType property : propertiesTerm) {
+								if (property.getValue().startsWith("C:")) { // cellular
+									// component
+									final String termName = property.getValue().substring(2);
+									if (sources == null) {
+										final GoTerm goTerm = new GoTermImpl();
+										final GoIdImpl goId = new GoIdImpl();
+										goId.setValue(dbReference.getId());
+										goTerm.setGoId(goId);
+										goTerm.setGoCategory(GoCategory.C);
+										final GoTermNameImpl goTermName = new GoTermNameImpl();
+										goTermName.setValue(termName);
+										goTerm.setGoTermName(goTermName);
+										ret.add(goTerm);
+
+									} else {
+										boolean found = false;
+										for (final String source : sources) {
+											for (final PropertyType propertyProject : propertiesProject) {
+												if (source.equals(propertyProject.getValue())) {
+													found = true;
+													break;
+												}
+											}
+										}
+										if (found) {
+											final GoTerm goTerm = new GoTermImpl();
+											final GoIdImpl goId = new GoIdImpl();
+											goId.setValue(dbReference.getId());
+											goTerm.setGoId(goId);
+											goTerm.setGoCategory(GoCategory.C);
+											final GoTermNameImpl goTermName = new GoTermNameImpl();
+											goTermName.setValue(termName);
+											goTerm.setGoTermName(goTermName);
+											ret.add(goTerm);
+										}
+									}
+								}
+
+							}
+						}
+					}
+				}
+			}
+		}
+		return ret;
 	}
 }
