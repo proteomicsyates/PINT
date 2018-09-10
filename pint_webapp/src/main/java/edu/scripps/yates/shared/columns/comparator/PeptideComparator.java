3edb50ac632223b4f74f5252a18f9cedbd6d2b90
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2018-09-10 14:48:14 -0700

    use of secondary non null parameters to compare strings

--- pint_webapp/src/main/java/edu/scripps/yates/shared/columns/comparator/PeptideComparator.java
+++ pint_webapp/src/main/java/edu/scripps/yates/shared/columns/comparator/PeptideComparator.java
@@ -51,9 +51,9 @@ public class PeptideComparator extends BeanComparator<PeptideBean> {
 			// if the column name doesn't fit with any other column , it is
 			// because
 			// it is a experimental condition name
-			String amountString1 = DataGridRenderValue
+			final String amountString1 = DataGridRenderValue
 					.getAmountDataGridRenderValue(o1, conditionName, amountType, projectTag).getValue();
-			String amountString2 = DataGridRenderValue
+			final String amountString2 = DataGridRenderValue
 					.getAmountDataGridRenderValue(o2, conditionName, amountType, projectTag).getValue();
 			return compareNumberStrings(amountString1, amountString2);
 
@@ -78,24 +78,28 @@ public class PeptideComparator extends BeanComparator<PeptideBean> {
 			try {
 				switch (columnName) {
 				case ACC:
-					return compareStrings(o1.getProteinAccessionString(), o2.getProteinAccessionString(), false);
+					return compareStrings(o1.getProteinAccessionString(), o2.getProteinAccessionString(), false,
+							o1.getFullSequence(), o2.getFullSequence());
 				case PEPTIDE_SEQUENCE:
 					return o1.getSequence().compareTo(o2.getSequence());
 				case PEPTIDE_LENGTH:
 					return Integer.compare(o1.getSequence().length(), o2.getSequence().length());
 
 				case POSITION_IN_PROTEIN:
-					int position1 = getMinPosition(o1);
-					int position2 = getMinPosition(o2);
+					final int position1 = getMinPosition(o1);
+					final int position2 = getMinPosition(o2);
 					return Integer.compare(position1, position2);
 
 				case TAXONOMY:
-					return compareStrings(o1.getOrganismsString(), o2.getOrganismsString(), true);
+					return compareStrings(o1.getOrganismsString(), o2.getOrganismsString(), true, o1.getFullSequence(),
+							o2.getFullSequence());
 
 				case CONDITION:
-					return compareStrings(o1.getConditionsString(), o2.getConditionsString(), true);
+					return compareStrings(o1.getConditionsString(), o2.getConditionsString(), true,
+							o1.getFullSequence(), o2.getFullSequence());
 				case PEPTIDE_EVIDENCE:
-					return compareStrings(o1.getRelation().name(), o2.getRelation().name(), true);
+					return compareStrings(o1.getRelation().name(), o2.getRelation().name(), true, o1.getFullSequence(),
+							o2.getFullSequence());
 				case SPECTRUM_COUNT:
 					return Integer.compare(o1.getNumPSMs(), o2.getNumPSMs());
 				case SPC_PER_CONDITION:
@@ -109,15 +113,19 @@ public class PeptideComparator extends BeanComparator<PeptideBean> {
 				case PEPTIDE_PTM:
 					final String[] uniprotFeatures = UniprotFeatures.getUniprotFeaturesByColumnName(columnName);
 					if (uniprotFeatures != null && uniprotFeatures.length > 0) {
-						return SharedDataUtils.getUniprotFeatureString(o1, uniprotFeatures)
-								.compareTo(SharedDataUtils.getUniprotFeatureString(o2, uniprotFeatures));
+						final String uniprotFeatureString1 = SharedDataUtils.getUniprotFeatureString(o1,
+								uniprotFeatures);
+						final String uniprotFeatureString2 = SharedDataUtils.getUniprotFeatureString(o2,
+								uniprotFeatures);
+						return compareStrings(uniprotFeatureString1, uniprotFeatureString2, true, o1.getFullSequence(),
+								o2.getFullSequence());
 					} else {
 						return 0;
 					}
 				default:
 					break;
 				}
-			} catch (NullPointerException e) {
+			} catch (final NullPointerException e) {
 
 			}
 		}
@@ -127,9 +135,9 @@ public class PeptideComparator extends BeanComparator<PeptideBean> {
 	private int getMinPosition(PeptideBean o1) {
 		int min = Integer.MAX_VALUE;
 		final Map<String, List<Pair<Integer, Integer>>> startingPositions = o1.getStartingPositions();
-		for (List<Pair<Integer, Integer>> positions : startingPositions.values()) {
-			for (Pair<Integer, Integer> startAndEnd : positions) {
-				int position = startAndEnd.getFirstElement();
+		for (final List<Pair<Integer, Integer>> positions : startingPositions.values()) {
+			for (final Pair<Integer, Integer> startAndEnd : positions) {
+				final int position = startAndEnd.getFirstElement();
 				if (min > position)
 					min = position;
 			}
@@ -146,10 +154,10 @@ public class PeptideComparator extends BeanComparator<PeptideBean> {
 		if ("".equals(string2) || string2 == null)
 			tmp2 = "0";
 		try {
-			Double d1 = Double.valueOf(tmp1);
-			Double d2 = Double.valueOf(tmp2);
+			final Double d1 = Double.valueOf(tmp1);
+			final Double d2 = Double.valueOf(tmp2);
 			return d1.compareTo(d2);
-		} catch (NumberFormatException e) {
+		} catch (final NumberFormatException e) {
 			return tmp1.compareTo(tmp2);
 		}
 
